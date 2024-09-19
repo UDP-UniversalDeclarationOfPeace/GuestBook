8fb709f49a01edf3e4c7e1c81d7288b0d28d1f18
Valentín Martínez <valentecreativo@proton.me>
2024-09-19 01:24:27 -0600

    Guestbook Smart Contract on Core Testnet successful compilation

---
+++ hardhat.config.js
@@ -0,0 +1,42 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+require("@nomicfoundation/hardhat-toolbox");
+
+const { PrivateKey } = require('./secret.json');
+
+module.exports = {
+   defaultNetwork: 'testnet',
+
+   networks: {
+      hardhat: {
+      },
+      testnet: {
+         url: 'https://rpc.test.btcs.network',
+         accounts: [PrivateKey],
+         chainId: 1115,
+      }
+   },
+   solidity: {
+      compilers: [
+        {
+           version: '0.8.9',
+           settings: {
+              optimizer: {
+                 enabled: true,
+                 runs: 200,
+              },
+           },
+        },
+      ],
+   },
+   paths: {
+      sources: './contracts',
+      cache: './cache',
+      artifacts: './artifacts',
+   },
+   mocha: {
+      timeout: 20000,
+   },
+};
